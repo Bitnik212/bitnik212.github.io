5717c719c9b9bcaf82c563ad310630e695e89486
Bit <minikraft1212@gmail.com>
2020-07-17 20:12:31 +0300

    Change chcolor.js and some fix in index.html

--- chcolor.js
+++ chcolor.js
@@ -1,33 +1,35 @@
-
-colors = ['#FF4040', '#FFE600', '#6ECA12', '#40A3FF', '#102ED0', '#E537BF']
-fio = document.getElementById('fio')
+debugg = false;
+timers = [];
+colors = ['#FF4040', '#FFE600', '#6ECA12', '#40A3FF', '#102ED0', '#E537BF'];
+fio = document.getElementById('fio');
 function rainbow(status) {
-    console.log('first = '+status)
+    if(debugg) console.log('status = '+status);
+    timers.push(chcolors())
+    if(debugg)
+        console.log('now timer = '+timers);
+    if (status === false){
+        if(debugg)
+            console.warn('stop timer = '+(timers));
+        for (let i = 0; i < timers.length; i++ )
+            clearInterval(timers[i])
+        timers = [];
+        fio.style.color = 'black'
+    }
+}
+function chcolors() {
     let timer;
-    let i = 0
+    let i = 0;
     timer = setInterval(function () {
         if (i < colors.length && status !== false) {
-            fio.style.color = colors[i]
-            console.log(colors[i])
-            i++
-            if(i == colors.length){
-                i = 0
-            }
+            fio.style.color = colors[i];
+            i++;
+            if(i === colors.length){i = 0;}
         }else{
             clearInterval(timer)
             fio.style.color = 'black'
         }
-        console.log('now timer = '+timer)
-
-    }, 500)
-
-    // console.log(status+' = prikol')
-    // console.log('onmouseover')
-
-    if (status === false){
-        console.log('stop timer = '+(timer -1))
-        // console.log('stop timer')
-        clearInterval(timer - 1)
-        fio.style.color = 'black'
-    }
+        if(debugg)
+            console.log('now timer in timer = '+timer);
+    }, 500);
+    return timer;
 }
