cdbd84dc13b155ef977bbd036780eae658cbdaa2
Bit <minikraft1212@gmail.com>
2020-07-20 12:05:39 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let cookies_alert = document.getElementById('cookie_alert');
+let cookies_alert = document.getElementById('cookie_alert_close'),
+    cookies_altalert = document.getElementById('cookie_alert_altclose'),
+    fix = document.getElementById("fixCookes");
 function cookie_alert_close() {
-    cookies_alert.style.display = 'none';
+    cookies_alert.style.display = cookies_altalert.style.display = fix.style.display = "none";
+}
+function cookie_alert_return() {
+    cookies_alert.style.display = cookies_altalert.style.display = fix.style.display = "flex";
 }
