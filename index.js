38ebd79901cc6e1cc4af011d8f98847176ce7caa
Bit <minikraft1212@gmail.com>
2020-12-14 07:01:28 +0300

    добавил экран и бг для них

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let cookies_alert = document.getElementById('cookie_alert_close'),
     cookies_altalert = document.getElementById('cookie_alert_altclose'),
-    fix = document.getElementById("fixCookes");
+    fix = document.getElementById("fixCookes"),
+    gg = document.getElementsByClassName("scrollBox_two_bg")[0];
 function cookie_alert_close() {
     cookies_alert.style.display = cookies_altalert.style.display = fix.style.display = "none";
+    gg.style.marginTop = "-580px";
 }
 function cookie_alert_return() {
     cookies_alert.style.display = cookies_altalert.style.display = fix.style.display = "flex";
+    gg.style.marginTop = "-600px";
 }
